54748909bb93405bb40fc1e437a00d2dee303f61
Rafael Moreira Fonseca <ramofojabuka@gmail.com>
2023-07-29 16:11:55 -0300

    feat: runner now shows the current folder

--- src/DevFolder/Runner.cs
+++ src/DevFolder/Runner.cs
@@ -4,6 +4,7 @@ using DevFolder.Exceptions;
 using DevFolder.Verbs;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.IO.Abstractions;
 
 namespace DevFolder;
 
@@ -11,16 +12,19 @@ public class Runner
 {
     private readonly CommandLineParseResult _commandLineParseResult;
     private readonly IServiceProvider _serviceProvider;
+    private readonly IFileSystem _fileSystem;
     private readonly ILogger _logger;
 
     public Runner(
         ILogger<Runner> logger,
         IServiceProvider serviceProvider,
-        CommandLineParseResult commandLineParseResult)
+        CommandLineParseResult commandLineParseResult,
+        IFileSystem fileSystem)
     {
         _logger = logger;
         _serviceProvider = serviceProvider;
         _commandLineParseResult = commandLineParseResult;
+        _fileSystem = fileSystem;
     }
 
     public async Task RunAsync()
@@ -29,6 +33,8 @@ public class Runner
         {
             throw new InvalidEnvArgumentsException("Invalid environment arguments.");
         }
+
+        _logger.LogInformation($"Current folder: \"{_fileSystem.Directory.GetCurrentDirectory()}\".");
 
         if (_commandLineParseResult.CloneVerbInstance is not null)
         {
